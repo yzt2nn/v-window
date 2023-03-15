437b63e37d7e4c1528e5f3a547f8c23536d86345
yzt <lovenekomusume@163.com>
2023-03-16 01:23:28 +0800

    修复bug。

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,20 +1,30 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 
 const msg = ref('You did it!')
-const clickMsg = ()=>{
+const clickMsg = () => {
   // msg.value += '1'
 }
 </script>
 
 <template>
   <header>
-    <img v-window="'image'" alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
+    <img
+      v-window="'image'"
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+      style="margin-top: 100px"
+    />
 
-    <div class="wrapper">
-      <HelloWorld v-window="'message'" @click="clickMsg" :msg="msg" style="padding: 50px;"/>
+    <div v-window="'message'">{{ msg }}</div>
+
+    <div class="wrapper" v-window="'message'">
+      <HelloWorld v-window="'message'" @click="clickMsg" :msg="msg" style="padding: 50px" />
     </div>
   </header>
 
@@ -28,26 +38,8 @@ header {
   line-height: 1.5;
 }
 
-.logo {
+/* .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+} */
 </style>
